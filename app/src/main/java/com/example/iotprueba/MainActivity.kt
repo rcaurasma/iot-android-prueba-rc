d72cafc995f4c6e0c8be34fc76da061194d87b5b
rcaurasma <rcaurasma>
2025-11-18 18:48:44 -0300

    App funcional ver 1

--- app/src/main/java/com/example/iotprueba/MainActivity.kt
+++ app/src/main/java/com/example/iotprueba/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import com.example.iotprueba.nav.AppNavGraph
 import com.example.iotprueba.ui.theme.IoTPruebaTheme
 
-class MainActivity : ComponentActivity() {2
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
