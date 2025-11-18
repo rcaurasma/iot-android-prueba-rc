e4f2c72e32e4cd33062c66ad8aaa57a4591ef3f5
rcaurasma <rcaurasma@gmail.com>
2025-11-18 20:24:47 -0300

    version con logo

--- app/src/main/java/com/example/iotprueba/screens/LoginScreen.kt
+++ app/src/main/java/com/example/iotprueba/screens/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.iotprueba.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -18,10 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.iotprueba.R
 import com.example.iotprueba.nav.Route
 import com.example.iotprueba.ui.theme.IoTPruebaTheme
 
@@ -40,6 +44,16 @@ fun LoginContent(
             .padding(20.dp),
         verticalArrangement = Arrangement.Center
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo1),
+            contentDescription = "Logo de la Aplicación",
+            modifier = Modifier
+                .size(150.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(Modifier.height(32.dp))
+
         Text("Bienvenido", fontSize = 23.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
         Spacer(Modifier.height(12.dp))
         OutlinedTextField(
