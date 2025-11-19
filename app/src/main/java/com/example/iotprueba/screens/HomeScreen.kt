e5e1008a5b5a3286451d939dd4a4302797e69975
rcaurasma <rcaurasma>
2025-11-19 16:09:57 -0300

    Icono, nombre de aplicacion, y preview en homescreen

--- app/src/main/java/com/example/iotprueba/screens/HomeScreen.kt
+++ app/src/main/java/com/example/iotprueba/screens/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -13,4 +14,10 @@ fun HomeScreen() {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text("¡Hola, usuario!", fontSize = 22.sp)
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
--- app/src/main/java/com/example/iotprueba/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/iotprueba/screens/RegisterScreen.kt
@@ -16,28 +16,48 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.iotprueba.nav.Route
 
 @Composable
 fun RegisterScreen(nav: NavController) {
     var name by remember { mutableStateOf("") }
+    var lastname by remember { mutableStateOf("") }
+    var phone by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var pwd by remember { mutableStateOf("") }
-    Column(Modifier.fillMaxSize().padding(20.dp), verticalArrangement =
-        Arrangement.Center) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(20.dp), verticalArrangement =
+        Arrangement.Center
+    ) {
         Text("Crear cuenta", fontSize = 22.sp)
-        OutlinedTextField(name, { name = it }, label = { Text(text = "Nombre") })
+        OutlinedTextField(name, { name = it }, label = { Text(text = "Nombre") }, modifier = Modifier.fillMaxWidth())
         Spacer(Modifier.height(8.dp))
-        OutlinedTextField(email, { email = it }, label = { Text("Correo") })
+        OutlinedTextField(lastname, { lastname = it }, label = { Text(text = "Apellido") }, modifier = Modifier.fillMaxWidth())
         Spacer(Modifier.height(8.dp))
-        OutlinedTextField(pwd, { pwd = it }, label = { Text("Contraseña") })
+        OutlinedTextField(phone, { phone = it }, label = { Text(text = "Teléfono") }, modifier = Modifier.fillMaxWidth())
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(email, { email = it }, label = { Text("Correo") }, modifier = Modifier.fillMaxWidth())
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(pwd, { pwd = it }, label = { Text("Contraseña") }, modifier = Modifier.fillMaxWidth())
         Spacer(Modifier.height(16.dp))
-        Button(onClick = { nav.navigate(Route.Login.path) }, modifier =
-            Modifier.fillMaxWidth()) {
+        Button(
+            onClick = { nav.navigate(Route.Login.path) }, modifier =
+            Modifier.fillMaxWidth()
+        ) {
             Text("Crear cuenta")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen(nav = rememberNavController())
 }
